5b95c96611f7f6cf2299735d5a3f1d4dd5f5ac86
Sahilwagh142 <sahil.wagh@digiplusit.com>
2026-02-07 07:42:12 +0530

    cors updated

--- app/main.py
+++ app/main.py
@@ -20,6 +20,7 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://192.168.0.109:3000",
+    "https://lead-gen-seven.vercel.app"
     "https://lead-generation-web-clients.vercel.app",
 ]
 
